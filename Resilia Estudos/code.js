33e0aea788766ae68ac9c799057cc8ecea40f30b
Aslan Campos <113717097+AslanDev28@users.noreply.github.com>
2022-10-18 00:20:48 -0300

    Add files via upload

---
+++ Resilia Estudos/code.js
@@ -0,0 +1,35 @@
+function clicar() {
+    var inputText = document.querySelector("input")
+    var btn = document.querySelector('button')
+    if(btn = inputText = document.querySelector("input").value){
+        alert('O resultado é: ' + inputText.charCodeAt())
+    }
+}
+
+
+
+        
+    
+    
+    
+    
+    
+    
+
+
+
+
+
+
+
+   
+    
+        
+        
+
+
+
+
+
+    
+          
